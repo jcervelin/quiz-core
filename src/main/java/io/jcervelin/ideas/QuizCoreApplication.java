a9f972ce04283cb6752a42885d06e4d031c29e19
Juliano de Sousa Cervelin <juliano.cervelin@sysmap.com.br>
2018-01-24 15:46:44 -0200

    Initial Commit

---
+++ src/main/java/io/jcervelin/ideas/QuizCoreApplication.java
@@ -0,0 +1,12 @@
+package io.jcervelin.ideas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizCoreApplication.class, args);
+	}
+}
